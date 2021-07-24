d4b899dbe28c9ba88754debbb94240e9807eee72
blake-hodges <tbhodges@gmail.com>
2021-07-24 17:21:13 +0800

    add fallback photo for background

--- src/Background.js
+++ src/Background.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import rainyDay from './rainy-day.mp4';
+import rainyDayPhoto from './rainy-day.jpg';
 import './css/Background.css';
 
 function Background() {
@@ -7,6 +8,7 @@ function Background() {
         <div>
         <video autoPlay loop muted className="rain-video">
             <source src={rainyDay} type="video/mp4" />
+            <img src={rainyDayPhoto} />
         </video>
     </div>
     )
